95f8391cc0e0d1eb5c7caa76b4740728d220e5d5
Niels Schuler <niels.schuler@web.de>
2022-09-13 12:07:11 +0200

    Added X movement

--- GeneralJump/Assets/Player/Scripts/movement.cs
+++ GeneralJump/Assets/Player/Scripts/movement.cs
@@ -1,9 +1,12 @@
+using Microsoft.Win32.SafeHandles;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class movement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector2(transform.position.x + (horizontal * speed), transform.position.y);
     }
 }
