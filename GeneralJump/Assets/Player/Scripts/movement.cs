6aff14bdb93596005619446fa5b46d5266b1b01a
Niels Schuler <niels.schuler@web.de>
2022-09-14 11:37:23 +0200

    Installed Input Debugger

--- GeneralJump/Assets/Player/Scripts/movement.cs
+++ GeneralJump/Assets/Player/Scripts/movement.cs
@@ -2,37 +2,34 @@ using Microsoft.Win32.SafeHandles;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class movement : MonoBehaviour
 {
-    public float speed;
-    public float jumpHeight;
-    public float gravity;
-    CharacterController controller;
+    public float movementSpeed;
+    public string horizontalAxisName;
+    public float jumpAxisName;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        float xAxs = Input.GetAxis(horizontalAxisName);
 
-        if (controller.isGrounded)
+        if(xAxs != 0)
         {
-            move.y = Input.GetAxis("Jump") * jumpHeight;
+            Vector3 position = gameObject.transform.position;
+
+            position.x = xAxs * movementSpeed;
+
+            gameObject.transform.position = position;
         }
-        else
-        {
-            move.y = transform.position.y - gravity;
-        }
+
         
-        controller.Move(move);
 
-        //X Y Z
+        
     }
 }
